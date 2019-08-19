ba2268cddd6dce8061baae107f912248ff4c4668
vladmog <vladmog@gmail.com>
2019-08-19 15:34:41 -0700

    Configured GET (no auth) and POST endpoints

--- api/api-model.js
+++ api/api-model.js
@@ -3,5 +3,24 @@ const config = require('../knexfile');
 const db = knex(config.development);
 
 module.exports = {
+    find,
+    add
 }
 
+function find(){
+    return db('users')
+}
+
+function findById(id){
+    return db('users')
+    .where({ id: id})
+    .first()
+}
+
+function add(credentials){
+    return db('users')
+    .insert(credentials)
+        .then(([id]) => {
+            return findById(id)
+        })
+}
